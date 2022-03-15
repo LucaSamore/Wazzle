7f655d4210f457e873e41be5e73d705de5c1fe07
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-03-15 21:44:43 +0100

    Test new branch

--- app/src/main/java/wazzle/App.java
+++ app/src/main/java/wazzle/App.java
@@ -7,7 +7,7 @@ import javafx.application.Application;
 
 public class App {
     public String getGreeting() {
-        return "Hello World!";
+        return "Hello World! It's a test :)";
     }
 
     public static void main(String[] args) {
