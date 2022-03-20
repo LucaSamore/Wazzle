262ed0bcc2e6a0a4220e3029651a1e1b1068a935
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-03-20 13:24:49 +0100

    Add letter allocator test and implementation

--- app/src/main/java/wazzle/model/maingame/LetterAllocatorImpl.java
+++ app/src/main/java/wazzle/model/maingame/LetterAllocatorImpl.java
@@ -1,7 +1,49 @@
 package wazzle.model.maingame;
 
+import java.util.EnumMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import javafx.util.Pair;
+
 public class LetterAllocatorImpl implements LetterAllocator {
 	
+	private EnumMap<Range, List<Pair<Character, Double>>> choosenLetters;
 	
-
+	public LetterAllocatorImpl(EnumMap<Range, List<Pair<Character, Double>>> choosenLetters) {
+		this.choosenLetters = new EnumMap<Range, List<Pair<Character, Double>>>(choosenLetters);
+	}
+	
+	public Set<Letter> alloc(){
+		//4 - RIMANDO IL CONTROLLO FINO A QUANDO LA GRIGLIA DOPO IL CONTROLLO NON SARA' 
+		//DI DIMENSIONI UGUALI A QUELLE DELLA GRIGLIa
+		
+		System.out.println(this.getSubgridDimension(Stream.of(Range.values())
+				  .map(Range::getWeight)
+				  .reduce(0, (x, y) -> x + y)));
+		return new HashSet<Letter>();
+	}
+	
+	private Pair<Integer, Integer> getSubgridDimension(int totalWeight) {
+		return (int) Math.sqrt(totalWeight) * (int) Math.sqrt(totalWeight) == totalWeight ? 
+				new Pair<>((int) Math.sqrt(totalWeight), (int) Math.sqrt(totalWeight)) :
+				new Pair<>((int) Math.sqrt(totalWeight), totalWeight/(int) Math.sqrt(totalWeight));
+	}
+	
+	private void findSubgrid(int gridDimension, int totalWeight) {
+		//1 - DIVIDO IN SOTTOGRIGLIE DI DIMENSIONI QUADRATE O RETTANGOLARI (SQRT(DIMGRIGLIA)) CHE ABBIANO 
+		//TANTE CELLE QUANTO LA SOMMA DEI PESI DEI RANGE	
+	}
+	
+	private void extractLetter() {
+		//2 - PER OGNI SOTTOGRIGLIA ESTRAGGO CASUALMENTE LE SUE LETTERE IN QTA = PESO DEI RANGE, 
+		//FACENDO ATTENZIONE A NON SOVRASCRIVERE QUELLE GIA MESSE
+	}
+	
+	private void checkGrid() {
+		//3 - AL TERMINE DELLA GENERAZIONE CASUALE FACCIO UN CONTROLLO CHE ELIMINA LE CELLE IN PIU' DI OGNI TIPO
+	}
 }
--- app/src/test/java/wazzle/model/TestLetterAllocatorImpl.java
+++ app/src/test/java/wazzle/model/TestLetterAllocatorImpl.java
@@ -2,8 +2,10 @@ package wazzle.model;
 
 import static org.junit.Assert.*;
 
+
 import java.util.Arrays;
 import java.util.EnumMap;
+import java.util.LinkedList;
 import java.util.Map;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -13,15 +15,43 @@ import java.util.List;
 import org.junit.Test;
 
 import javafx.util.Pair;
+import wazzle.model.maingame.Letter;
+import wazzle.model.maingame.LetterImpl;
 import wazzle.model.maingame.LetterAllocator;
 import wazzle.model.maingame.LetterAllocatorImpl;
+import wazzle.model.maingame.LetterImpl;
 import wazzle.model.maingame.Range;
+import wazzle.model.maingame.WeightedAlphabet;
+import wazzle.model.maingame.WeightedAlphabetImpl;
 
 public class TestLetterAllocatorImpl {
 
 	@Test
 	public void testAllocator() {
-
+		EnumMap<Range, List<Pair<Character, Double>>> choosenLetters = new EnumMap<Range, List<Pair<Character, Double>>> 
+				(Map.of(Range.HIGH_FREQUENCY, List.of(new Pair<>('A', 10.5), new Pair<>('E', 8.9), 
+						new Pair<>('I', 10.0), new Pair<>('0', 9.0), 
+						new Pair<>('A', 10.5), new Pair<>('E', 8.9), 
+						new Pair<>('I', 10.0), new Pair<>('0', 9.0)), 
+				Range.MEDIUM_FREQUENCY, List.of(new Pair<>('C', 8.5), new Pair<>('T', 8.8), 
+						new Pair<>('R', 8.2), new Pair<>('R', 7.0), 
+						new Pair<>('S', 8.4)), 
+				Range.LOW_FREQUENCY, List.of(new Pair<>('N', 3.0), new Pair<>('X', 0.5), 
+						new Pair<>('Z', 1.0))));
+		
+		LetterAllocator letterAllocator = new LetterAllocatorImpl(choosenLetters);
+		Set<Letter> allocatedLetters = letterAllocator.alloc();
+		
+//		List<Pair<Character, Double>> allocatedLettersTotal = new LinkedList<>();
+//		allocatedLetters.forEach(v -> allocatedLettersTotal.add(new Pair<Character, Double>(v.getContent(), v.getPosition())));
+//		List<Pair<Character, Double>> choosenLettersTotal = new LinkedList<>();
+//		choosenLetters.forEach((k,v) -> choosenLettersTotal.addAll(v));
+//		assertEquals(choosenLettersTotal, allocatedLettersTotal);
+		
+//		Set<Letter> allocatedLetters = Set.of(new LetterImpl('I', new Pair<>(0,0)), new LetterImpl('O', new Pair<>(0,1)), new LetterImpl('R', new Pair<>(0,2)), new LetterImpl('R', new Pair<>(0,3)), 
+//				new LetterImpl('I', new Pair<>(1,0)), new LetterImpl('I', new Pair<>(1,1)), new LetterImpl('I', new Pair<>(1,2)), new LetterImpl('I', new Pair<>(1,3)), 
+//				new LetterImpl('I', new Pair<>(2,0)), new LetterImpl('I', new Pair<>(2,1)), new LetterImpl('I', new Pair<>(2,2)), new LetterImpl('I', new Pair<>(2,3)),
+//				new LetterImpl('I', new Pair<>(3,0)), new LetterImpl('I', new Pair<>(3,1)), new LetterImpl('I', new Pair<>(3,2)), new LetterImpl('I', new Pair<>(3,3)));
 		fail("Not yet implemented");
 	}
 
