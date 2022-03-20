76958eb3e88c012adfb70925a42922067db74264
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-03-20 13:27:38 +0100

    Update Range

--- app/src/main/java/wazzle/model/maingame/Range.java
+++ app/src/main/java/wazzle/model/maingame/Range.java
@@ -1,19 +1,19 @@
 package wazzle.model.maingame;
 
 public enum Range {
-	
+
 	HIGH_FREQUENCY(3),
-	
+
 	MEDIUM_FREQUENCY(2),
-	
+
 	LOW_FREQUENCY(1);
-	
+
 	int weight;
-	
+
 	Range(int weight) {
 		this.weight = weight;
 	}
-	
+
 	public int getWeight() {
 		return this.weight;
 	}
