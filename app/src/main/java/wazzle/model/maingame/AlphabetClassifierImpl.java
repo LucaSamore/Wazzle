533b03f17e9d944e8f7d4acbc0877c6a732f4df7
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-03-25 20:54:33 +0100

    Update AlphabetClassifier

--- app/src/main/java/wazzle/model/maingame/AlphabetClassifierImpl.java
+++ app/src/main/java/wazzle/model/maingame/AlphabetClassifierImpl.java
@@ -12,17 +12,18 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 
 
-public class AlphabetClassifierImpl implements AlphabetClassifier {
+public final class AlphabetClassifierImpl implements AlphabetClassifier {
 	
 	private WeightedAlphabet weightedAlphabet;
-	private Function<EnumMap<Range, Map<Character, Double>>, Map<Range, WeightedAlphabet>> f = m -> m.entrySet()
+	private final Function<EnumMap<Range, Map<Character, Double>>, Map<Range, WeightedAlphabet>> f = m -> m.entrySet()
 	   .stream()
 	   .collect(Collectors.toMap(Map.Entry::getKey, e -> new WeightedAlphabetImpl(e.getValue())));
 	
-	public AlphabetClassifierImpl (WeightedAlphabet weightedAlphabet) {
+	public AlphabetClassifierImpl (final WeightedAlphabet weightedAlphabet) {
 		this.weightedAlphabet = new WeightedAlphabetImpl(weightedAlphabet.getWeightedAlphabet());
 	}
 	
+	@Override
 	public EnumMap<Range, WeightedAlphabet> classify() {
 		EnumMap <Range, Map<Character, Double>> classifiedLetters = new EnumMap<>(Range.class);
 		Arrays.asList(Range.values()).stream().collect(Collectors.toSet()).forEach(r -> classifiedLetters.put(r, new HashMap<>()));
@@ -31,11 +32,11 @@ public class AlphabetClassifierImpl implements AlphabetClassifier {
 		return new EnumMap<>(f.apply(classifiedLetters));
 	}
 	
-	private Range chooseRange(EnumMap <Range, Map<Character, Double>> classifiedLetters) {
+	private Range chooseRange(final EnumMap <Range, Map<Character, Double>> classifiedLetters) {
 		return classifiedLetters.keySet().stream().filter(k -> !this.isFill(classifiedLetters, k)).collect(Collectors.toList()).get(0);
 	}
 	
-	private boolean isFill (EnumMap <Range, Map<Character, Double>> classifiedLetters, Range range) {
+	private boolean isFill (final EnumMap <Range, Map<Character, Double>> classifiedLetters, final Range range) {
 		return classifiedLetters.get(range).values().stream().reduce((double) 0, Double::sum) >= this.computeRangeQuote();
 	}
 	
@@ -43,7 +44,7 @@ public class AlphabetClassifierImpl implements AlphabetClassifier {
 		return this.weightedAlphabet.getWeightedAlphabet().values().stream().reduce((double) 0, Double::sum)/Range.values().length;
 	}
 
-	private List<Entry<Character, Double>> sortWeightedAlphabethByValue (List<Map.Entry<Character, Double>> weightedAlphabetList) {
+	private List<Entry<Character, Double>> sortWeightedAlphabethByValue (final List<Map.Entry<Character, Double>> weightedAlphabetList) {
 		Collections.sort(weightedAlphabetList, ((v1, v2) -> v2.getValue().compareTo(v1.getValue())));
 		return weightedAlphabetList;
 	}
