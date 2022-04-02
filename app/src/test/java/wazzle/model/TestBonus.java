31a1e0f169c48c57bfc5e089a2314d6938e0fbbc
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-04-02 11:36:58 +0200

    Bug fix in BonusManager

--- app/src/test/java/wazzle/model/TestBonus.java
+++ app/src/test/java/wazzle/model/TestBonus.java
@@ -15,23 +15,29 @@ public class TestBonus {
 	public void testBonus() {
 		BonusManager bonusManager = new BonusManagerImpl();
 		
-		bonusManager.updateScoreBonusQuantity(s -> s+1);
+		//ScoreBonus
+		bonusManager.updateScoreBonusQuantity(b -> b+1);
 		assertEquals(bonusManager.getScoreBonusQuantity(), 1);
-		bonusManager.updateWordBonusQuantity(s -> s+3);
-		
-		assertEquals(bonusManager.getWordBonusQuantity(), 3);
-		bonusManager.updateWordBonusQuantity(s -> s-1);
-		assertEquals(bonusManager.getWordBonusQuantity(), 2);
-		
 		var currentScore = 1000;
 		var gridTotalScore = 100;
 		assertTrue(bonusManager.applyScoreBonus(currentScore, gridTotalScore) == currentScore + gridTotalScore);
+		assertEquals(bonusManager.getScoreBonusQuantity(), 0);
 		
+		//WordBonus
+		bonusManager.updateWordBonusQuantity(b -> b+3);
+		assertEquals(bonusManager.getWordBonusQuantity(), 3);
+		bonusManager.updateWordBonusQuantity(b -> b-1);
+		assertEquals(bonusManager.getWordBonusQuantity(), 2);
+		Set<String> toFoundWords = Set.of("cane", "gatto", "topo", "ape", "libellula", "rana");
+		assertTrue(toFoundWords.containsAll(bonusManager.applyWordBonus(toFoundWords)));
+		assertEquals(bonusManager.getWordBonusQuantity(), 1);
+		
+		//TimeBonus
+		bonusManager.updateTimeBonusQuantity(b -> b+2);
+		assertEquals(bonusManager.getTimeBonusQuantity(), 2);	
 		long halfMinute = 30000;
 		long increment = 120000;
 		assertTrue(bonusManager.applyTimeBonus(increment) == halfMinute + increment);
-		
-		Set<String> toFoundWords = Set.of("cane", "gatto", "topo", "ape", "libellula", "rana");
-		assertTrue(toFoundWords.containsAll(bonusManager.applyWordBonus(toFoundWords)));
+		assertEquals(bonusManager.getTimeBonusQuantity(), 1);	
 	}
 }
