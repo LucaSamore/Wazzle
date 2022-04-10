0571f8b5b4753d38ab73be8a2a6fadf827710564
Lucia Castellucci <lucia.castellucci2@studio.unibo.it>
2022-04-10 14:49:55 +0200

    Add Launcher, start App implementation and update its test, update build.gradle.kts

--- app/src/main/java/wazzle/App.java
+++ app/src/main/java/wazzle/App.java
@@ -3,14 +3,39 @@
  */
 package wazzle;
 
+import java.io.IOException;
+import java.net.URL;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import wazzle.view.controller.MainMenuView;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.geometry.Rectangle2D;
 
-public class App {
-    public String getGreeting() {
-        return "Hello World! It's a test :)";
+public class App extends Application {
+	
+	private static final String SEPARATOR = System.getProperty("file.separator");
+
+	public void start(Stage stage) throws IOException {
+			Rectangle2D screenViewport = Screen.getPrimary().getBounds();
+			stage.setWidth(screenViewport.getWidth()*0.75);
+			stage.setHeight(screenViewport.getHeight()*0.75);
+			MainMenuView mainMenuController = new MainMenuView(stage);
+			FXMLLoader loader = new FXMLLoader();
+			loader.setLocation(new URL("file:" + System.getProperty("user.dir") + SEPARATOR + "src" + SEPARATOR + 
+					"main" + SEPARATOR + "res" + SEPARATOR + "layouts" + SEPARATOR + "MainMenu.fxml"));
+			loader.setController(mainMenuController);
+			Parent root = loader.load();
+			Scene scene = new Scene(root);
+			stage.setScene(scene);
+			stage.show();
+	}
+	
+    public static void main(final String[] args) {
+        launch();
     }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
 }
---
+++ app/src/main/java/wazzle/Launcher.java
@@ -0,0 +1,11 @@
+package wazzle;
+
+public class Launcher {
+	
+    private Launcher() { }
+
+    public static void main(final String[] args) {
+        App.main(args);
+    }
+
+}
