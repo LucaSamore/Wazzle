46d912174facffdf98873d693dc4e104e830db12
Alessio Barbanti <alessiokyra@gmail.com>
2022-04-17 19:21:17 +0200

    first release

---
+++ app/src/main/java/wazzle/controller/minigame/WordDispenser.java
@@ -0,0 +1,9 @@
+package wazzle.controller.minigame;
+
+import wazzle.controller.common.WazzleController;
+
+public interface WordDispenser {
+
+	public String getSuitableWord(WazzleController mainController);
+
+}
