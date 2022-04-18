c23336028880459b4f27a1c84618f6e705ebf9e2
Alessio Barbanti <alessiokyra@gmail.com>
2022-04-18 10:28:51 +0200

    moved and deleted some files

--- app/src/main/java/wazzle/controller/minigame/WordDispenser.java
+++
@@ -1,9 +0,0 @@
-package wazzle.controller.minigame;
-
-import wazzle.controller.common.WazzleController;
-
-public interface WordDispenser {
-
-	public String getSuitableWord(WazzleController mainController);
-
-}
