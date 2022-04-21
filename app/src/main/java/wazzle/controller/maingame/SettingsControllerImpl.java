ff3993778c1ebdad2b0b82850c00c525ac51cfbf
lucas <luca.samore@studio.unibo.it>
2022-04-22 00:46:10 +0200

    Reimplement settings controller (looking fresh now)

--- app/src/main/java/wazzle/controller/maingame/SettingsControllerImpl.java
+++ app/src/main/java/wazzle/controller/maingame/SettingsControllerImpl.java
@@ -2,12 +2,16 @@ package wazzle.controller.maingame;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import org.checkerframework.checker.nullness.qual.NonNull;
 
 import com.google.gson.annotations.Expose;
 
 import wazzle.model.maingame.Difficulty;
-
-//TODO setcurrent difficulty to default
 
 public class SettingsControllerImpl implements SettingsController {
 	@Expose
@@ -16,8 +20,9 @@ public class SettingsControllerImpl implements SettingsController {
 	@Expose
 	private Difficulty currentDifficulty;
 
-	public SettingsControllerImpl(final List<Difficulty> difficulties) {
+	public SettingsControllerImpl(@NonNull final List<Difficulty> difficulties, @NonNull final Difficulty currentDifficulty) {
 		this.difficulties = new ArrayList<>(difficulties);
+		this.currentDifficulty = currentDifficulty;
 	}
 
 	@Override
@@ -31,7 +36,16 @@ public class SettingsControllerImpl implements SettingsController {
 	}
 
 	@Override
-	public void setCurrentDifficulty(final Difficulty difficulty) {
+	public void setCurrentDifficulty(@Nullable final Difficulty difficulty) {
+		Objects.requireNonNull(difficulty);
 		this.currentDifficulty = difficulty;
 	}
+
+	@Override
+	public Optional<Difficulty> getDifficultyByNameAndShape(final String name, final int shape) {
+		return this.difficulties.stream()
+				.filter(d -> d.getDifficultyName().equals(name) && d.getGridShape() == shape)
+				.findAny();
+	}
+
 }
