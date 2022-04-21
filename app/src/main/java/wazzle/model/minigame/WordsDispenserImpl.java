db16647b189c0a2d1c4fd50df00baf1efd0ba015
Alessio Barbanti <alessio.barbanti@studio.unibo.it>
2022-04-21 15:19:48 +0200

    removed some comments from file

--- app/src/main/java/wazzle/model/minigame/WordsDispenserImpl.java
+++ app/src/main/java/wazzle/model/minigame/WordsDispenserImpl.java
@@ -1,16 +1,10 @@
 package wazzle.model.minigame;
 
-import java.io.IOException;
 import java.util.Random;
-
-import wazzle.controller.common.FileController;
-import wazzle.controller.common.FileControllerImpl;
-import wazzle.controller.common.WazzleController;
 
 public final class WordsDispenserImpl implements WordsDispenser {
 	
 	private static final Random RANDOM = new Random();
-
 	private final Comparator availableWordManager;
 	
 	public WordsDispenserImpl(final Comparator availableWordManager) {
@@ -20,31 +14,11 @@ public final class WordsDispenserImpl implements WordsDispenser {
 
 	@Override
 	public String extractWord() {
-
 		if (!this.availableWordManager.areThereAnyAvailableWords()) {
 			this.availableWordManager.restoreAvailableWords();
 		}
 		var extraction = this.availableWordManager.getAvailableWords().toArray()[RANDOM.nextInt(this.availableWordManager.getAvailableWords().size()-1)].toString();
 		this.availableWordManager.notAvailableAnymore(extraction);
 		return extraction;
-
-//		this.availableWordManager.getAvailableWords().stream().findAny().ifPresentOrElse(w -> word = w , () -> this.availableWordManager.restoreAvailableWords());
-//		return this.availableWords.toArray()[RANDOM.nextInt(availableWords.size())].toString();
-		
-//		FileController fileController = new FileControllerImpl();
-//		
-//		Comparator comparator = new ComparatorImpl(fileController.getDataset("minigameDataset.txt"));
-//		/**
-//		 * check if the dataset containing all the words is empty, and in case it does, update it.
-//		 * 
-//		 */
-//		if (comparator.isDatasetEmpty()) {
-//			comparator.refreshDataSet(new FiveLetterDictionary(fileController.getDataset("dataset.txt")));
-//		}
-//		
-//		String suitableWord = comparator.getSuitableWord();
-//		comparator.removeFromSavedDataset(suitableWord);
-//		comparator.saveDataSet();/* gli passo il dataset da aggiornare (JSON) */
-//		return suitableWord;
 	}
 }
