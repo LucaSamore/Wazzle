16cb1bdba51844badc4dcffec5cb0b53af946f48
LucaSamore <luca.samore.01@gmail.com>
2022-04-23 01:18:56 +0200

    Add javadoc to WordPathChecker

--- app/src/main/java/wazzle/model/maingame/WordPathChecker.java
+++ app/src/main/java/wazzle/model/maingame/WordPathChecker.java
@@ -2,13 +2,24 @@ package wazzle.model.maingame;
 
 import java.util.HashMap;
 import java.util.Map;
-
 import javafx.util.Pair;
 
-public class WordPathChecker {
+/**
+ * This class provides an algorithm for searching a {@code String} inside a {@code Map<Pair<Integer, Integer>, Character>}.
+ * This class is used by {@link Filters#checkPath(String, java.util.Set)}
+ * All credits for this implementation go to <i>jump1221</i> (thank you buddy):
+ * @see <b>source:</b> https://leetcode.com/problems/word-search/discuss/27811/My-Java-solution
+ */
+public final class WordPathChecker {
 	
 	static Map<Pair<Integer, Integer>, Boolean> visited = new HashMap<>();
 	
+	/**
+	 * Checks if a given word exists inside a {@code Map<Pair<Integer, Integer>, Character>}.
+	 * @param board {@code Map<Pair<Integer, Integer>, Character>} the given grid.
+	 * @param word {@code String} the given word.
+	 * @return {@code true} if the word exists inside the map, {@code false} otherwise.
+	 */
 	public boolean exist(final Map<Pair<Integer, Integer>, Character> board, final String word) {
 		visited.clear();
         for (int i = 0; i < Math.sqrt(board.size()); i++) {
