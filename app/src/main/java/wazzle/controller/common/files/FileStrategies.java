ef9ecc93de9403f723b04bca1e890fde1f571264
LucaSamore <luca.samore.01@gmail.com>
2022-04-23 08:36:33 +0200

    More javadoc to file handling classes and interfaces

--- app/src/main/java/wazzle/controller/common/files/FileStrategies.java
+++ app/src/main/java/wazzle/controller/common/files/FileStrategies.java
@@ -6,15 +6,61 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.List;
 
+/**
+ * This interface provides methods for performing operations on a file.
+ *
+ * @param <X> the type of object we want to perform a file operation with.
+ */
 public interface FileStrategies<X> {
+	
+	/**
+	 * Reads a file given his path
+	 * @param path a {@code String} representing the path of the file.
+	 * @param <X> the type of object we want to perform the read operation with.
+	 * @return a {@code List<X>} containing the elements read from the file.
+	 * @throws IOException
+	 * @see java.util.List
+	 */
 	List<X> read(final String path) throws IOException;
 	
+	/**
+	 * Reads a file given an {@link InputStream}.
+	 * @param stream a {@code InputStream} representing the file.
+	 * @param <X> the type of object we want to perform the read operation with.
+	 * @return a {@code List<X>} containing the elements read from the file.
+	 * @throws IOException
+	 * @see java.util.List
+	 */
 	List<X> read(final InputStream stream) throws IOException;
 	
+	/**
+	 * Writes the provided content to a file in write mode, given the path. Content will be overwritten.
+	 * @param path a {@code String} representing the path.
+	 * @param toBeWritten a {@code List<X>} representing the content we want to write.
+	 * @param <X> the type of object we want to perform the write operation with.
+	 * @return void
+	 * @throws IOException
+	 * @see java.util.List
+	 */
 	void write(final String path, final List<X> toBeWritten) throws IOException;
 	
+	/**
+	 * Writes the provided content to a file in append mode, given the path.
+	 * @param path a {@code String} representing the path.
+	 * @param toBeAdded a {@code List<X>} representing the content we want to append.
+	 * @param <X> the type of object we want to perform the append operation with.
+	 * @return void
+	 * @throws IOException
+	 * @see java.util.List
+	 */
 	void append(final String path, final List<X> toBeAdded) throws IOException;
 	
+	/**
+	 * Clears the content of a file given his path.
+	 * @param path a {@code String} representing the path.
+	 * @return void
+	 * @throws IOException
+	 */
 	default void clear(final String path) throws IOException {
 		Files.delete(Path.of(path));
 		Files.createFile(Path.of(path));
--- app/src/main/java/wazzle/controller/common/files/Serializer.java
+++ app/src/main/java/wazzle/controller/common/files/Serializer.java
@@ -5,11 +5,18 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.LocalDateTime;
 import java.util.function.Supplier;
-
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 
+/**
+ * This interface provides a method for objects serialization to a .json file.
+ */
 public interface Serializer {
+	
+	/**
+	 * Supplies a custom {@link Gson} object used for serialization.
+	 * @return a {@link Supplier} for {@code Gson} objects}.
+	 */
 	default Supplier<Gson> serializationGson() {
 		return () -> new GsonBuilder()
 				.setPrettyPrinting()
@@ -19,6 +26,14 @@ public interface Serializer {
 				.create();
 	}
 	
+	/**
+	 * Serializes a sequence of generic objects to a .json file, given the path.
+	 * @param path a {@code String} representing the path of the file.
+	 * @param toBeWritten the elements we want to serialize.
+	 * @param <X> the type of the objects to be serialized.
+	 * @return void
+	 * @throws IOException
+	 */
 	@SuppressWarnings("unchecked")
 	default <X> void serialize(final String path, final X... toBeWritten) throws IOException {
 		try(final var writer = new FileWriter(path, StandardCharsets.UTF_8)) {
