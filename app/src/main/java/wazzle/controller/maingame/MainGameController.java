202a395d893f5e8e60abe938bb3bf3bafe955738
LucaSamore <luca.samore.01@gmail.com>
2022-04-23 02:09:16 +0200

    Add javadoc to MainGameController and MainGameControllerImpl

--- app/src/main/java/wazzle/controller/maingame/MainGameController.java
+++ app/src/main/java/wazzle/controller/maingame/MainGameController.java
@@ -2,37 +2,97 @@ package wazzle.controller.maingame;
 
 import java.util.Optional;
 import java.util.Set;
-
 import javafx.util.Pair;
 import wazzle.controller.common.WazzleController;
 import wazzle.model.common.Dictionary;
 import wazzle.model.maingame.Difficulty;
 import wazzle.model.maingame.MainGame;
 
+/**
+ * This interface represents the controller for {@link MainGame}.
+ * It provides methods for handling every state of the game and gain information about it.
+ */
 public interface MainGameController {
+	
+	/**
+	 * Returns an {@code Optional} describing the current game.
+	 * @return an {@code Optional} object.
+	 */
 	Optional<MainGame> getGame();
 	
+	/**
+	 * This method starts a new game session by requesting a new {@link MainGame} instance.
+	 * @param dataset a {@link Dictionary} object used for creating the {@link Grid}
+	 * @param gridShape a {@code Pair<Integer,Integer> object that represents the shape of the {@link Grid}
+	 * @param difficulty a {@link Difficulty} object chosen by the user
+	 * @return void.
+	 */
 	void startNewGame(final Dictionary dataset, final Pair<Integer,Integer> gridShape, final Difficulty difficulty);
 	
+	/**
+	 * This method handles the end game situation.
+	 * @return void.
+	 */
 	void endGame();
 	
+	/**
+	 * This method calls {@link MainGame#tryWord(String)} and returns his result.
+	 * @param word {@code String} the word to try
+	 * @return {@code true} if {@link MainGame#tryWord(String)} returns {@code true}, {@code false} otherwise.
+	 */
 	boolean attempt(final String word);
 	
+	/**
+	 * This method tells if the user needs help by checking how many failed attemps he got in row.
+	 * @return {@code true} if {@link MainGame#getFailedAttempts()} equals a certain number, {@code false} otherwise.
+	 */
 	boolean needHelp();
 	
+	/**
+	 * This method checks if the game is over.
+	 * @return {@code true} if {@link MainGame#wordsFound()} == {@link MainGame#wordsCanBeFound()}, {@code false} otherwise.
+	 */
 	boolean areWeDone();
 	
+	/**
+	 * Consumes a score bonus. It increases {@link MainGame#getCurrentScore()}.
+	 * @return void.
+	 */
 	void useScoreBonus();
 	
+	/**
+	 * Consumes a time bonus. It increases the timer by using the {@link GameTimer} object.
+	 * @return void.
+	 */
 	void useTimeBonus();
 	
+	/**
+	 * Consumes a word bonus.
+	 * @return a {@code Set<String>} containing some suggested words inside {@link MainGame#wordsToBeFound()}.
+	 */
 	Set<String> useWordBonus();
 	
+	/**
+	 * Returns the {@link WazzleController} object.
+	 * @return a {@code WazzleController} object.
+	 */
 	WazzleController getMainController();
 	
+	/**
+	 * Returns the longest word inside {@link MainGame#wordsFound()}.
+	 * @return a {@code String} representing the longest word.
+	 */
 	String longestWord();
 	
+	/**
+	 * Returns the word with the highest score inside {@link MainGame#wordsFound()}.
+	 * @return a {@code String} representing the word with the highest score.
+	 */
 	String highestScoreWord();
 	
+	/**
+	 * Returns the {@code GameTimer} object.
+	 * @return a {@link GameTimer} object.
+	 */
 	GameTimer getTimer();
 }
--- app/src/main/java/wazzle/controller/maingame/MainGameControllerImpl.java
+++ app/src/main/java/wazzle/controller/maingame/MainGameControllerImpl.java
@@ -1,22 +1,27 @@
 package wazzle.controller.maingame;
 
 import java.util.Comparator;
-
 import java.util.Optional;
 import java.util.Set;
-
 import javafx.util.Pair;
 import wazzle.controller.common.WazzleController;
 import wazzle.model.common.Dictionary;
 import wazzle.model.maingame.Difficulty;
 import wazzle.model.maingame.MainGame;
 
+/**
+ * This class is a concrete implementation for {@link MainGameController}
+ */
 public final class MainGameControllerImpl implements MainGameController {
 	private static final int ATTEMPTS_BEFORE_HELP = 5;
 	private final WazzleController mainController;
 	private Optional<MainGame> game;
 	private final GameTimer timer;
 	
+	/**
+	 * Construct a new MainGameControllerImpl.
+	 * @param mainController a {@link WazzleController} object.
+	 */
 	public MainGameControllerImpl(final WazzleController mainController) {
 		this.mainController = mainController.getThis();
 		this.game = Optional.empty();
@@ -27,11 +32,17 @@ public final class MainGameControllerImpl implements MainGameController {
 			      );
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Optional<MainGame> getGame() {
 		return this.game;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void startNewGame(final Dictionary dataset, final Pair<Integer,Integer> gridShape, final Difficulty difficulty) {
 		if(this.game.isEmpty()) {
@@ -39,6 +50,9 @@ public final class MainGameControllerImpl implements MainGameController {
 		}
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void endGame() {
 		this.game.ifPresent(g -> {
@@ -47,6 +61,9 @@ public final class MainGameControllerImpl implements MainGameController {
 		});
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean attempt(final String word) {
 		if(this.game.isPresent()) {
@@ -55,16 +72,25 @@ public final class MainGameControllerImpl implements MainGameController {
 		return false;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean needHelp() {
 		return this.game.get().getFailedAttempts() == ATTEMPTS_BEFORE_HELP;
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public boolean areWeDone() {
 		return this.game.get().wordsFound().size() == this.game.get().wordsCanBeFound().size();
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void useScoreBonus() {
 		this.game.get().setCurrentScore(this.mainController
@@ -72,27 +98,35 @@ public final class MainGameControllerImpl implements MainGameController {
 				.applyScoreBonus(this.game.get().getCurrentScore(), this.game.get().getGrid().getTotalScore()));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public void useTimeBonus() {
 		this.timer.updateTime(this.mainController.getBonusManager().applyTimeBonus(this.timer.getRemainingTime()));
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public Set<String> useWordBonus() {
 		return this.mainController
 				.getBonusManager()
 				.applyWordBonus(this.game.get().wordsToBeFound());
 	}
-	
-	private void saveGame() {
-		this.mainController.addMainGametoHistory(this.game.get());
-	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public WazzleController getMainController() {
 		return this.mainController;
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String longestWord() {
 		return this.game.get()
@@ -102,6 +136,9 @@ public final class MainGameControllerImpl implements MainGameController {
 				.orElse("");
 	}
 
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public String highestScoreWord() {		
         double max = this.game.get()
@@ -119,8 +156,15 @@ public final class MainGameControllerImpl implements MainGameController {
                 .get();
 	}
 	
+	/**
+	 * {@inheritDoc}
+	 */
 	@Override
 	public GameTimer getTimer() {
 		return this.timer;
+	}
+	
+	private void saveGame() {
+		this.mainController.addMainGametoHistory(this.game.get());
 	}
 }
