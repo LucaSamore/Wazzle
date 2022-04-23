28f3e8e306f51b7017ce98c1146d77eecca24ba0
LucaSamore <luca.samore.01@gmail.com>
2022-04-23 11:48:20 +0200

    Finish tutorial view

--- app/src/main/java/wazzle/view/controller/TutorialMainGameView.java
+++ app/src/main/java/wazzle/view/controller/TutorialMainGameView.java
@@ -1,72 +1,68 @@
 package wazzle.view.controller;
 
 import java.io.IOException;
-
 import javafx.beans.binding.Bindings;
-import javafx.beans.property.DoubleProperty;
 import javafx.beans.property.SimpleDoubleProperty;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
+import wazzle.controller.common.WazzleController;
+import wazzle.view.FXMLFiles;
 import wazzle.view.Images;
+import wazzle.view.SceneSwitcher;
 
-public final class TutorialMainGameView {
-	@FXML
-	private ImageView gifTutorial;
-
-	@FXML
-	private Label textLabel;
-
-	@FXML
-	private VBox contentWrapper;
-
-	@FXML
-	private VBox mainWrapper;
-
-	@FXML
-	private Button exitButton;
-	
-	@FXML 
-	private VBox elementWrapper;
-	
-	private DoubleProperty visualUnit;
+public final class TutorialMainGameView extends View<WazzleController> {
+	@FXML private ImageView gifTutorial;
+	@FXML private TextArea textArea;
+	@FXML private VBox contentWrapper;
+	@FXML private VBox mainWrapper;
+	@FXML private Button exitButton;
+	@FXML private VBox elementWrapper;
 	
 	public TutorialMainGameView(final Stage stage) {
+		this.stage = stage;
 		this.visualUnit = new SimpleDoubleProperty();
 		visualUnit.bind(Bindings.min(stage.heightProperty(), stage.widthProperty()));
 	}
 	
-	public void initialize() {
-		this.setGraphics();		
+	@Override
+	public void nextScene(final ActionEvent event) throws IOException {
+		SceneSwitcher.<MainMenuView>switchScene(event, new MainMenuView(this.stage), FXMLFiles.MAIN_MENU.getPath());		
 	}
 	
-	public void nextScene(ActionEvent event) throws IOException {
-		// TODO Auto-generated method stub		
+	@Override
+	protected void buildView() {
+		this.setGraphics();
 	}
 
+	@Override
 	protected void setGraphics() {
 		final var fontSize = Bindings.concat("-fx-font-size: ", visualUnit.multiply(0.05).asString(), ";");
 		final var textFontSize = Bindings.concat("-fx-font-size: ", visualUnit.multiply(0.025).asString(), ";");
 		final var paddingValue = Bindings.concat("-fx-padding: ", visualUnit.multiply(0.05).asString(), ";");
 		final var spacingValue = Bindings.concat("-fx-spacing: ", visualUnit.multiply(0.02).asString(), ";");
 		final var explanationGif = new Image(Images.TUTORIAL_MAIN_GAME.getPath());
-		elementWrapper.styleProperty().bind(spacingValue);
-		contentWrapper.maxHeightProperty().bind(contentWrapper.widthProperty());
-		contentWrapper.styleProperty().bind(Bindings.concat(paddingValue, spacingValue));
-		gifTutorial.setImage(explanationGif);
-		gifTutorial.fitWidthProperty().bind(visualUnit.multiply(0.7));
-		gifTutorial.fitHeightProperty().bind(visualUnit.multiply(0.7));
+		this.elementWrapper.styleProperty().bind(spacingValue);
+		this.contentWrapper.maxHeightProperty().bind(contentWrapper.widthProperty());
+		this.contentWrapper.styleProperty().bind(Bindings.concat(paddingValue, spacingValue));
+		this.gifTutorial.setImage(explanationGif);
+		this.gifTutorial.fitWidthProperty().bind(visualUnit.multiply(0.7));
+		this.gifTutorial.fitHeightProperty().bind(visualUnit.multiply(0.7));
 		this.writeTutorialToLabel();
-		textLabel.styleProperty().bind(Bindings.concat(textFontSize, paddingValue));
-		exitButton.styleProperty().bind(fontSize);
+		this.textArea.styleProperty().bind(Bindings.concat(textFontSize, paddingValue));
+		this.exitButton.styleProperty().bind(fontSize);
 	}
 	
 	private void writeTutorialToLabel() {
-		
+		this.textArea.setText("Lo scopo del gioco è quello di cercare il maggior numero di "
+				+ "parole all'interno del waffle. La parola si colorerà di verde "
+				+ "se è corretta e di rosso se sbagliata. Durante il gioco è possibile "
+				+ "usufruire di tre diversi bonus quali aumento del tempo, "
+				+ "aumento del punteggio e suggerimento di parole.");
 	}
 }
